ec8017ed780ff93d5185ba11b6da0b4f14d4b372
ArtyomGorbackij <31548827+ArtyomGorbackij@users.noreply.github.com>
2017-09-29 23:13:47 +0300

    Create ar.cpp

---
+++ ar.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main( )
+{int max,max2;
+    for( string string; getline( cin, string ); ) {
+        int numbers[ 10 ];
+        istringstream stream( string );
+        bool failure = false;
+        for( int i = 0; i < 10; ++i ) {
+            if( !( stream >> numbers[ i ] ) ) {
+                failure = true;
+                break;
+            }
+        }
+         max=numbers[0];
+        if( !failure ) {
+            for( int i = 1; i < 10; ++i ) {
+                if(max<numbers[i]){max=numbers[i];}
+            }
+            break;
+        }
+        else {
+           cout << "An error has occured while reading numbers from line" << endl;
+        }
+    }
+       for( string string; getline( cin, string ); ) {
+        int numb[ 10 ];
+        istringstream stream( string );
+        bool failure = false;
+        for( int i = 0; i < 10; ++i ) {
+            if( !( stream >> numb[ i ] ) ) {
+                failure = true;
+                break;
+            }
+        }
+        
+        if( !failure ) {
+            for( int i = 1; i < 10; ++i ) {
+                max2=numb[0];
+                if(max2<numb[i]){max2=numb[i];}
+            }
+            break;
+        }
+        else {
+           cout << "An error has occured while reading numbers from line" << endl;
+           
+        } 
+      }cout<<max+max2;
+ }
